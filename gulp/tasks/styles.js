60abdfcd0e2f8c7617a6b28b1a5ae20cdc02ce9f
Kasper Worm Andersson <k.wormandersson@gmail.com>
2017-04-04 16:11:42 +0200

    styleguide

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,9 +1,14 @@
 'use strict';
-var gulp = require('gulp');
-var config = require('../config');
-var sass = require('gulp-sass')
 
-gulp.task('build', ['clean'], function() {
+var browserSync = require('browser-sync');
+var config = require('../config');
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var postcss = require('gulp-postcss')
+var sass = require('gulp-sass');
+var styleGuide = require('postcss-style-guide');
+
+gulp.task('scss', function() {
 	return gulp.src(config.paths.entry)
 		.pipe(sass({
 			includePaths: [
@@ -11,6 +16,29 @@ gulp.task('build', ['clean'], function() {
 				'bower_components/'
 			],
 		})).on('error', sass.logError)
-		.pipe(gulp.dest(config.paths.dest));
+		.pipe(postcss([
+			styleGuide({
+				themePath: 'node_modules/psg-theme-minimal',
+				project: 'Hive Frontend',
+				dest: 'dist/index.html',
+				showCode: true,
+			})
+		]))
+		.pipe(gulpif(browserSync.active, browserSync.reload({
+			stream: true
+		})));
 });
 
+gulp.task('serve', ['scss'], function(){
+	browserSync.init({
+		server: {
+			baseDir: config.paths.dest
+		}
+	});
+});
+
+gulp.task('build', ['serve'], function() {
+	// Start watching changes and update styleguide whenever changes are detected
+	// Styleguide automatically detects existing server instance
+	gulp.watch([config.paths.all], ['scss']);
+});
